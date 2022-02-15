c8031095e7fbcf392ad8ce4f5de3d3b6f79ed9c7
Chase <sccala18@gmail.com>
2022-02-15 12:40:21 -0800

    clearAll

--- App.js
+++ App.js
@@ -49,7 +49,7 @@ export default function App() {
   }
 
   const onClickClearAll = () => {
-    setDeleteTodos((deleteTodos) => !deleteTodos)
+    setDeleteTodos([])
   }
 
   const onClickComplete = (index) => {
